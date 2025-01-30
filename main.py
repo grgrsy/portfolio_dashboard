22aa25f057282d8257c9e78a747ac3111df4425e
grgrsy <196646363+grgrsy@users.noreply.github.com>
2025-01-30 18:26:06 +0100

    creation de deux object pour les tests

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.data_treatment import *
+from src.streamlit_backend import *
+
+
+wallet = Wallet("data/PEA-orders.xlsx")
+prices = Price('data/PEA-cours_historique_cache - Copie.xlsx')
+
+
+
+
