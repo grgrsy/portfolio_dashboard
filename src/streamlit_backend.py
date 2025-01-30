0e1e6dbc27a1cc6e4098a2bb5995cc3a70075f46
grgrsy <196646363+grgrsy@users.noreply.github.com>
2025-01-30 18:25:30 +0100

    creation fichier streamlit backend, v0

---
+++ src/streamlit_backend.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def page_config():
+    st.set_page_config(
+        page_title = "Portfolio Dashboard",
+        layout="wide",
+        initial_sidebar_state="expanded"
+        )
